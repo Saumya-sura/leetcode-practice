699d7244556ecd6944171ae7c76c737d7606d9bd
saumya-sura <saumya.sura74@nmims.in>
2025-07-19 15:00:35 +0530

    postorder

--- trees/treerepresentation.cpp
+++ trees/treerepresentation.cpp
@@ -10,3 +10,37 @@ struct node{
             right=nullptr;
         }
 };
+void preorder(node *root){
+    if(root ==NULL) return ;
+    cout<<root->data<<" ";
+    preorder(root->left);
+    preorder(root->right);
+}
+void postorder(node *root){
+    if(root ==NULL) return ;
+    postorder(root->left);
+    postorder(root->right);
+    cout<<root->data<<" ";
+}
+void inorder(node *root){
+    if(root ==NULL) return ;
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+}   
+int main(){
+    node *root = new node(1);
+    root->left = new node(2);
+    root->right = new node(3);
+    root->left->left = new node(4);
+    root->left->right = new node(5);
+    root->right->left = new node(6);
+    root->right->right = new node(7);
+    preorder(root);
+    
+    cout << endl;
+    postorder(root);
+    cout << endl;
+    inorder(root);
+    return 0;
+}
