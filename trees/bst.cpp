c50d25745f80dcea7fe7e77fe5aeb588ca838cdc
saumya-sura <saumya.sura74@nmims.in>
2025-07-19 15:05:22 +0530

    Implement code changes to enhance functionality and improve performance

---
+++ trees/bst.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+using namespace std;
+struct node{
+        int data;
+        node *left;
+        node *right;
+        node(int val){
+            data =val;
+            left=nullptr;
+            right=nullptr;
+        }
+};
+void preorder(node *root){
+    if(root ==NULL) return ;
+    cout<<root->data<<" ";
+    preorder(root->left);
+    preorder(root->right);
+}
+void postorder(node *root){
+    if(root ==NULL) return ;
+    postorder(root->left);
+    postorder(root->right);
+    cout<<root->data<<" ";
+}
+void inorder(node *root){
+    if(root ==NULL) return ;
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+}   
+int  isBst(struct node*root){
+    static struct node* prev =NULL;
+    if(root!=NULL){
+        if(!isBst(root->left)) {
+        return false;
+        }
+        if(prev != NULL && prev->data >= root->data) {
+            return false;
+        }
+        prev = root;
+        return isBst(root->right);
+    }
+return 1;
+}
+int main(){
+    node *root = new node(1);
+    root->left = new node(2);
+    root->right = new node(3);
+    root->left->left = new node(4);
+    root->left->right = new node(5);
+    root->right->left = new node(6);
+    root->right->right = new node(7);
+    preorder(root);
+    
+    cout << endl;
+    postorder(root);
+    cout << endl;
+    inorder(root);
+    cout << endl;
+    if(isBst(root)){
+        cout << "The tree is a BST" << endl;
+    } else {
+        cout << "The tree is not a BST" << endl;
+    }
+    return 0;
+    
+}
